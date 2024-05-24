706ffd48fd375768d0834322bcecda03ce39e4f4
kangfenmao <kangfenmao@qq.com>
2024-05-24 16:32:26 +0800

    chore(lint): Update ESLint and Prettier configurations

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,5 +5,9 @@ module.exports = {
     'plugin:react/jsx-runtime',
     '@electron-toolkit/eslint-config-ts/recommended',
     '@electron-toolkit/eslint-config-prettier'
-  ]
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
+  }
 }
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,7 @@
 import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
-import icon from '@/renderer/resources/icon.png?asset'
+import icon from '../../resources/icon.png?asset'
 import windowStateKeeper from 'electron-window-state'
 
 function createWindow(): void {
