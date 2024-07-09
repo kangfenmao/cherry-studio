9b92b191aea9543ca391f5909e5bcb13556b6811
kangfenmao <kangfenmao@qq.com>
2024-07-09 16:26:10 +0800

    fix: highlighter not working

--- src/renderer/src/pages/home/components/CodeBlock.tsx
+++ src/renderer/src/pages/home/components/CodeBlock.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import SyntaxHighlighter from 'react-syntax-highlighter'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
 import styled from 'styled-components'
 import { CopyOutlined } from '@ant-design/icons'
