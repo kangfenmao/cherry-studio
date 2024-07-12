848797e824e8f4face158774d5129a181d27d9bc
kangfenmao <kangfenmao@qq.com>
2024-07-12 17:51:04 +0800

    build: disable devTools on prod mode

--- src/main/index.ts
+++ src/main/index.ts
@@ -28,7 +28,8 @@ function createWindow(): void {
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
-      sandbox: false
+      sandbox: false,
+      devTools: !app.isPackaged
     }
   })
 
