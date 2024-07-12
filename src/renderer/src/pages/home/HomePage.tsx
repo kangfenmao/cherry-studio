5f0f223b5b9cc2d0b6c7ec2e12542e89c6ffc5a6
kangfenmao <kangfenmao@qq.com>
2024-07-12 15:33:21 +0800

    fix: inputbar height

--- src/renderer/src/pages/home/HomePage.tsx
+++ src/renderer/src/pages/home/HomePage.tsx
@@ -54,7 +54,7 @@ const Container = styled.div`
   display: flex;
   flex: 1;
   flex-direction: column;
-  height: 100%;
+  height: calc(100vh - var(--navbar-height));
 `
 
 const ContentContainer = styled.div`
