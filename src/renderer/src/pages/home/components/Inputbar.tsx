de17df9f14a26ac23959fe114463f2f8caae0491
kangfenmao <kangfenmao@qq.com>
2024-07-14 11:46:25 +0800

    fix: inputbar translation

--- src/renderer/src/pages/home/components/Inputbar.tsx
+++ src/renderer/src/pages/home/components/Inputbar.tsx
@@ -135,8 +135,7 @@ const Inputbar: FC<Props> = ({ assistant, setActiveTopic }) => {
               description={t('assistant.input.clear.content')}
               placement="top"
               onConfirm={clearTopic}
-              okText="Clear"
-              cancelText="Cancel">
+              okText={t('assistant.input.clear')}>
               <ToolbarButton type="text">
                 <ClearOutlined />
               </ToolbarButton>
