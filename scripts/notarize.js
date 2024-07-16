a19c69340ac6ffbaae3e4694896ddc5499aaf434
kangfenmao <kangfenmao@qq.com>
2024-07-16 20:49:41 +0800

    ci: log notarized app name

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -12,14 +12,15 @@ exports.default = async function notarizing(context) {
   }
 
   const appName = context.packager.appInfo.productFilename
+  const appPath = `${context.appOutDir}/${appName}.app`
 
   await notarize({
-    appPath: `${context.appOutDir}/${appName}.app`,
+    appPath,
     appBundleId: 'com.kangfenmao.CherryStudio',
     appleId: process.env.APPLE_ID,
     appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
     teamId: process.env.APPLE_TEAM_ID
   })
 
-  console.log('Notarized app:', appName)
+  console.log('  • Notarized app:', appPath)
 }
