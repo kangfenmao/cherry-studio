e028d0600f1c8cced4b2e43e60bd05fd880c4e38
kangfenmao <kangfenmao@qq.com>
2024-08-08 23:30:55 +0800

    fix: windows style

--- src/renderer/src/config/constant.ts
+++ src/renderer/src/config/constant.ts
@@ -5,5 +5,5 @@ export const FONT_FAMILY =
   "Ubuntu, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"
 export const platform = window.electron?.process?.platform
 export const isMac = platform === 'darwin'
-export const isWindows = platform === 'windows'
+export const isWindows = platform === 'win32' || platform === 'win64'
 export const isLinux = platform === 'linux'
