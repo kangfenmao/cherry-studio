3d44fc220856d9f4eaffae1abd53e2a28eef5639
kangfenmao <kangfenmao@qq.com>
2024-08-08 14:50:36 +0800

    fix: navbar style on linux

--- src/renderer/src/config/constant.ts
+++ src/renderer/src/config/constant.ts
@@ -4,5 +4,6 @@ export const DEFAULT_MAX_TOKENS = 4096
 export const FONT_FAMILY =
   "Ubuntu, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"
 export const platform = window.electron?.process?.platform
-export const isMac = platform === 'darwin' || platform === 'linux'
+export const isMac = platform === 'darwin'
 export const isWindows = platform === 'windows'
+export const isLinux = platform === 'linux'
