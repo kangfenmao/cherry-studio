72e847258d85a1697ccebb08bbf9832d5ae4058c
kangfenmao <kangfenmao@qq.com>
2024-10-29 14:48:48 +0800

    feat: add instance lock and second instance handling

--- src/main/index.ts
+++ src/main/index.ts
@@ -7,6 +7,11 @@ import { registerZoomShortcut } from './shortcut'
 import { updateUserDataPath } from './utils/upgrade'
 import { createMainWindow } from './window'
 
+// Check for single instance lock
+if (!app.requestSingleInstanceLock()) {
+  app.quit()
+}
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
@@ -39,6 +44,15 @@ app.whenReady().then(async () => {
     installExtension(REDUX_DEVTOOLS)
       .then((name) => console.log(`Added Extension:  ${name}`))
       .catch((err) => console.log('An error occurred: ', err))
+  }
+})
+
+// Listen for second instance
+app.on('second-instance', () => {
+  const mainWindow = BrowserWindow.getAllWindows()[0]
+  if (mainWindow) {
+    mainWindow.isMinimized() && mainWindow.restore()
+    mainWindow.focus()
   }
 })
 
