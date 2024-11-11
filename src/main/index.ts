446e011c6ad48e289e0432943f7d28c4c3a09abc
kangfenmao <kangfenmao@qq.com>
2024-11-11 10:49:11 +0800

    chore(version): 0.8.12

--- src/main/index.ts
+++ src/main/index.ts
@@ -11,57 +11,59 @@ import { updateUserDataPath } from './utils/upgrade'
 // Check for single instance lock
 if (!app.requestSingleInstanceLock()) {
   app.quit()
-}
+  process.exit(0)
+} else {
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.whenReady().then(async () => {
+    await updateUserDataPath()
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(async () => {
-  await updateUserDataPath()
+    // Set app user model id for windows
+    electronApp.setAppUserModelId(import.meta.env.VITE_MAIN_BUNDLE_ID || 'com.kangfenmao.CherryStudio')
 
-  // Set app user model id for windows
-  electronApp.setAppUserModelId(import.meta.env.VITE_MAIN_BUNDLE_ID || 'com.kangfenmao.CherryStudio')
+    const mainWindow = windowService.createMainWindow()
+    new TrayService()
 
-  const mainWindow = windowService.createMainWindow()
-  new TrayService()
+    app.on('activate', function () {
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (BrowserWindow.getAllWindows().length === 0) {
+        windowService.createMainWindow()
+      } else {
+        windowService.showMainWindow()
+      }
+    })
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-      windowService.createMainWindow()
-    } else {
-      windowService.showMainWindow()
+    registerZoomShortcut(mainWindow)
+
+    registerIpc(mainWindow, app)
+
+    if (process.env.NODE_ENV === 'development') {
+      installExtension(REDUX_DEVTOOLS)
+        .then((name) => console.log(`Added Extension:  ${name}`))
+        .catch((err) => console.log('An error occurred: ', err))
     }
   })
 
-  registerZoomShortcut(mainWindow)
+  // Listen for second instance
+  app.on('second-instance', () => {
+    const mainWindow = BrowserWindow.getAllWindows()[0]
+    if (mainWindow) {
+      mainWindow.isMinimized() && mainWindow.restore()
+      mainWindow.show()
+      mainWindow.focus()
+    }
+  })
 
-  registerIpc(mainWindow, app)
+  app.on('browser-window-created', (_, window) => {
+    optimizer.watchWindowShortcuts(window)
+  })
 
-  if (process.env.NODE_ENV === 'development') {
-    installExtension(REDUX_DEVTOOLS)
-      .then((name) => console.log(`Added Extension:  ${name}`))
-      .catch((err) => console.log('An error occurred: ', err))
-  }
-})
+  app.on('before-quit', () => {
+    app.isQuitting = true
+  })
 
-// Listen for second instance
-app.on('second-instance', () => {
-  const mainWindow = BrowserWindow.getAllWindows()[0]
-  if (mainWindow) {
-    mainWindow.isMinimized() && mainWindow.restore()
-    mainWindow.focus()
-  }
-})
-
-app.on('browser-window-created', (_, window) => {
-  optimizer.watchWindowShortcuts(window)
-})
-
-app.on('before-quit', () => {
-  app.isQuitting = true
-})
-
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
+  // In this file you can include the rest of your app"s specific main process
+  // code. You can also put them in separate files and require them here.
+}
