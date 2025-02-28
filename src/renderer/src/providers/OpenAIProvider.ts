f489b034b54c57a89f9151718171b41415dc7a2b
kangfenmao <kangfenmao@qq.com>
2025-02-28 14:20:37 +0800

    Revert "feat: allow o1 streaming"
    This reverts commit d4848faa5aad692157844c640d193bce86cbeec3.

--- src/renderer/src/providers/OpenAIProvider.ts
+++ src/renderer/src/providers/OpenAIProvider.ts
@@ -197,7 +197,12 @@ export default class OpenAIProvider extends BaseProvider {
       userMessages.push(await this.getMessageParam(message, model))
     }
 
+    const isOpenAIo1 = this.isOpenAIo1(model)
+
     const isSupportStreamOutput = () => {
+      if (isOpenAIo1) {
+        return false
+      }
       return streamOutput
     }
 
@@ -327,8 +332,13 @@ export default class OpenAIProvider extends BaseProvider {
         ]
       : [{ role: 'user', content: assistant.prompt }]
 
+    const isOpenAIo1 = this.isOpenAIo1(model)
+
     const isSupportedStreamOutput = () => {
       if (!onResponse) {
+        return false
+      }
+      if (isOpenAIo1) {
         return false
       }
       return true
