a0351fb5adaf12158a6d839a0954fca0f667d7d2
kangfenmao <kangfenmao@qq.com>
2025-03-07 13:48:19 +0800

    chore: Add TypeScript ignore comment for provider property in BaseWebSearchProvider

--- src/renderer/src/webSearchProvider/BaseWebSearchProvider.ts
+++ src/renderer/src/webSearchProvider/BaseWebSearchProvider.ts
@@ -1,6 +1,7 @@
 import { WebSearchProvider, WebSearchResponse } from '@renderer/types'
 
 export default abstract class BaseWebSearchProvider {
+  // @ts-ignore this
   private provider: WebSearchProvider
   constructor(provider: WebSearchProvider) {
     this.provider = provider
