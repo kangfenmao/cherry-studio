a04757c0d9975794f7d12be44a06d89f1e7dcbb9
George·Dong <georgedong32@foxmail.com>
2025-03-15 00:38:56 +0800

    fix(NewContextButton): Incorrect relation to showInputEstimatedTokens setting

--- src/renderer/src/pages/home/Inputbar/NewContextButton.tsx
+++ src/renderer/src/pages/home/Inputbar/NewContextButton.tsx
@@ -1,5 +1,4 @@
 import { PicCenterOutlined } from '@ant-design/icons'
-import { useSettings } from '@renderer/hooks/useSettings'
 import { useShortcut, useShortcutDisplay } from '@renderer/hooks/useShortcuts'
 import { Tooltip } from 'antd'
 import { FC } from 'react'
@@ -13,13 +12,8 @@ interface Props {
 const NewContextButton: FC<Props> = ({ onNewContext, ToolbarButton }) => {
   const newContextShortcut = useShortcutDisplay('toggle_new_context')
   const { t } = useTranslation()
-  const { showInputEstimatedTokens } = useSettings()
 
   useShortcut('toggle_new_context', onNewContext)
-
-  if (!showInputEstimatedTokens) {
-    return null
-  }
 
   return (
     <Tooltip placement="top" title={t('chat.input.new.context', { Command: newContextShortcut })} arrow>
