4c6a90492993504a00bd658d8ac166d4e602535a
dcai <heydcai@gmail.com>
2025-04-24 23:39:35 +1000

    fix: Resolve unsafe map call in MessageContent.tsx (#5311)

--- src/renderer/src/pages/home/Messages/MessageContent.tsx
+++ src/renderer/src/pages/home/Messages/MessageContent.tsx
@@ -64,11 +64,14 @@ const MessageContent: React.FC<Props> = ({ message: _message, model }) => {
   // 获取引用数据
   // https://github.com/CherryHQ/cherry-studio/issues/5234#issuecomment-2824704499
   const citationsData = useMemo(() => {
+    const citationUrls =
+      Array.isArray(message.metadata?.citations) &&
+      (message?.metadata?.annotations?.map((annotation) => annotation.url_citation) ?? [])
     const searchResults =
       message?.metadata?.webSearch?.results ||
       message?.metadata?.webSearchInfo ||
       message?.metadata?.groundingMetadata?.groundingChunks?.map((chunk) => chunk?.web) ||
-      (message?.metadata?.annotations?.map((annotation) => annotation.url_citation) ?? []) ||
+      citationUrls ||
       []
 
     // 使用对象而不是 Map 来提高性能
