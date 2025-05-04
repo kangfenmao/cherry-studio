5ceb2af0563bbfb4d9443aa8c0d39aadf04262e6
beyondkmp <beyondkmp@gmail.com>
2025-05-04 18:34:43 +0800

    chore(dependencies): update electron-builder to 26.0.15 (#5651)
    * chore(dependencies): update electron-builder and related dependencies to version 26.0.15
    
    * chore(electron-builder): update portable build configuration and clean up artifact build script
    
    - Set buildUniversalInstaller to false for portable builds in electron-builder configuration.
    - Removed obsolete condition for deleting portable files in artifact build completion script.

--- scripts/artifact-build-completed.js
+++ scripts/artifact-build-completed.js
@@ -8,11 +8,6 @@ exports.default = function (buildResult) {
     }
 
     let oldFilePath = buildResult.file
-    if (oldFilePath.includes('-portable') && !oldFilePath.includes('-x64') && !oldFilePath.includes('-arm64')) {
-      console.log('[artifact build completed] delete portable file:', oldFilePath)
-      fs.unlinkSync(oldFilePath)
-      return
-    }
     const newfilePath = oldFilePath.replace(/ /g, '-')
     fs.renameSync(oldFilePath, newfilePath)
     buildResult.file = newfilePath
