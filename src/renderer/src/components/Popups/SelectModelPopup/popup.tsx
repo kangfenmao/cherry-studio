b9224ed311825929844122fbce328bfbeb94433b
one <wangan.cs@gmail.com>
2025-05-13 13:22:24 +0800

    fix: animation on resolving SelectModelPopup (#5947)

--- src/renderer/src/components/Popups/SelectModelPopup/popup.tsx
+++ src/renderer/src/components/Popups/SelectModelPopup/popup.tsx
@@ -256,12 +256,11 @@ const PopupContainer: React.FC<Props> = ({ model, resolve }) => {
   const handleItemClick = useCallback(
     (item: FlatListItem) => {
       if (item.type === 'model') {
-        setScrollTrigger('initial')
         resolve(item.model)
         setOpen(false)
       }
     },
-    [resolve, setScrollTrigger]
+    [resolve]
   )
 
   // 处理键盘导航
@@ -633,16 +632,7 @@ export class SelectModelPopup {
 
   static show(params: PopupParams) {
     return new Promise<Model | undefined>((resolve) => {
-      TopView.show(
-        <PopupContainer
-          {...params}
-          resolve={(v) => {
-            resolve(v)
-            TopView.hide(TopViewKey)
-          }}
-        />,
-        TopViewKey
-      )
+      TopView.show(<PopupContainer {...params} resolve={(v) => resolve(v)} />, TopViewKey)
     })
   }
 }
