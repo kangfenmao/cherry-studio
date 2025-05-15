e9d6b9f44188171831afe1993e2e5384fcb2bc8d
Song <1667077010@qq.com>
2025-05-15 22:07:10 +0800

    fix: fix portable dir setup time (#6022)

--- src/main/index.ts
+++ src/main/index.ts
@@ -40,6 +40,9 @@ if (!app.requestSingleInstanceLock()) {
   app.quit()
   process.exit(0)
 } else {
+  // Portable dir must be setup before app ready
+  setUserDataDir()
+  
   // This method will be called when Electron has finished
   // initialization and is ready to create browser windows.
   // Some APIs can only be used after this event occurs.
@@ -71,8 +74,6 @@ if (!app.requestSingleInstanceLock()) {
     registerIpc(mainWindow, app)
 
     replaceDevtoolsFont(mainWindow)
-
-    setUserDataDir()
 
     // Setup deep link for AppImage on Linux
     await setupAppImageDeepLink()
