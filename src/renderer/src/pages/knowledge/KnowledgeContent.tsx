a95ace3dc570e766c3d171bc6f72d44c583fba11
suyao <sy20010504@gmail.com>
2025-05-19 22:45:59 +0800

    fix: update file path resolution in new Electron

--- src/renderer/src/pages/knowledge/KnowledgeContent.tsx
+++ src/renderer/src/pages/knowledge/KnowledgeContent.tsx
@@ -93,7 +93,7 @@ const KnowledgeContent: FC<KnowledgeContentProps> = ({ selectedBase }) => {
         .map((file) => ({
           id: file.name,
           name: file.name,
-          path: file.path,
+          path: window.api.file.getPathForFile(file),
           size: file.size,
           ext: `.${file.name.split('.').pop()}`.toLowerCase(),
           count: 1,
