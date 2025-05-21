f9512d6ef277ce3858a2216c191a10709731a1ad
kangfenmao <kangfenmao@qq.com>
2025-05-21 22:38:57 +0800

    fix: handle user cancellation and improve error reporting in file saving process
    - Added a check for user cancellation in the file save dialog, rejecting the promise if canceled.
    - Enhanced error handling to reject the promise with a detailed error message instead of returning null.

--- src/main/services/FileStorage.ts
+++ src/main/services/FileStorage.ts
@@ -328,7 +328,7 @@ class FileStorage {
     fileName: string,
     content: string,
     options?: SaveDialogOptions
-  ): Promise<string | null> => {
+  ): Promise<string> => {
     try {
       const result: SaveDialogReturnValue = await dialog.showSaveDialog({
         title: '保存文件',
@@ -336,14 +336,18 @@ class FileStorage {
         ...options
       })
 
+      if (result.canceled) {
+        return Promise.reject(new Error('User canceled the save dialog'))
+      }
+
       if (!result.canceled && result.filePath) {
         await writeFileSync(result.filePath, content, { encoding: 'utf-8' })
       }
 
       return result.filePath
-    } catch (err) {
+    } catch (err: any) {
       logger.error('[IPC - Error]', 'An error occurred saving the file:', err)
-      return null
+      return Promise.reject('An error occurred saving the file: ' + err?.message)
     }
   }
 
