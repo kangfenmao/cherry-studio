7d6ef1d69ab4ea9ce0368e55fc2e75e094a46e5a
Pleasurecruise <3196812536@qq.com>
2025-05-22 17:34:23 +0800

    fix: handle empty block content in MessageTranslate component

--- src/renderer/src/pages/home/Messages/MessageTranslate.tsx
+++ src/renderer/src/pages/home/Messages/MessageTranslate.tsx
@@ -14,16 +14,12 @@ interface Props {
 const MessageTranslate: FC<Props> = ({ block }) => {
   const { t } = useTranslation()
 
-  if (!block.content) {
-    return null
-  }
-
   return (
     <Fragment>
       <Divider style={{ margin: 0, marginBottom: 10 }}>
         <TranslationOutlined />
       </Divider>
-      {block.content === t('translate.processing') ? (
+      {!block.content || block.content === t('translate.processing') ? (
         <SvgSpinners180Ring color="var(--color-text-2)" style={{ marginBottom: 15 }} />
       ) : (
         <Markdown block={block} />
