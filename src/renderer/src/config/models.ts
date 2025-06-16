1b5cba94d2af92b2a0397af8409b0c465340b945
自由的世界人 <3196812536@qq.com>
2025-06-16 23:44:11 +0800

    fix: modify siliconflow text-to-image available models (#7165)
    * fix: remove painting provider
    
    * Update PaintingsRoutePage.tsx
    
    * fix: text to image models

--- src/renderer/src/config/models.ts
+++ src/renderer/src/config/models.ts
@@ -2185,71 +2185,77 @@ export const SYSTEM_MODELS: Record<string, Model[]> = {
 
 export const TEXT_TO_IMAGES_MODELS = [
   {
-    id: 'black-forest-labs/FLUX.1-schnell',
+    id: 'Kwai-Kolors/Kolors',
     provider: 'silicon',
-    name: 'FLUX.1 Schnell',
-    group: 'FLUX'
-  },
-  {
-    id: 'black-forest-labs/FLUX.1-dev',
-    provider: 'silicon',
-    name: 'FLUX.1 Dev',
-    group: 'FLUX'
-  },
-  {
-    id: 'black-forest-labs/FLUX.1-pro',
-    provider: 'silicon',
-    name: 'FLUX.1 Pro',
-    group: 'FLUX'
-  },
-  {
-    id: 'Pro/black-forest-labs/FLUX.1-schnell',
-    provider: 'silicon',
-    name: 'FLUX.1 Schnell Pro',
-    group: 'FLUX'
-  },
-  {
-    id: 'LoRA/black-forest-labs/FLUX.1-dev',
-    provider: 'silicon',
-    name: 'FLUX.1 Dev LoRA',
-    group: 'FLUX'
-  },
-  {
-    id: 'deepseek-ai/Janus-Pro-7B',
-    provider: 'silicon',
-    name: 'Janus-Pro-7B',
-    group: 'deepseek-ai'
-  },
-  {
-    id: 'stabilityai/stable-diffusion-3-5-large',
-    provider: 'silicon',
-    name: 'Stable Diffusion 3.5 Large',
-    group: 'Stable Diffusion'
-  },
-  {
-    id: 'stabilityai/stable-diffusion-3-5-large-turbo',
-    provider: 'silicon',
-    name: 'Stable Diffusion 3.5 Large Turbo',
-    group: 'Stable Diffusion'
-  },
-  {
-    id: 'stabilityai/stable-diffusion-3-medium',
-    provider: 'silicon',
-    name: 'Stable Diffusion 3 Medium',
-    group: 'Stable Diffusion'
-  },
-  {
-    id: 'stabilityai/stable-diffusion-2-1',
-    provider: 'silicon',
-    name: 'Stable Diffusion 2.1',
-    group: 'Stable Diffusion'
-  },
-  {
-    id: 'stabilityai/stable-diffusion-xl-base-1.0',
-    provider: 'silicon',
-    name: 'Stable Diffusion XL Base 1.0',
-    group: 'Stable Diffusion'
+    name: 'Kolors',
+    group: 'Kwai-Kolors'
   }
+  // {
+  //   id: 'black-forest-labs/FLUX.1-schnell',
+  //   provider: 'silicon',
+  //   name: 'FLUX.1 Schnell',
+  //   group: 'FLUX'
+  // },
+  // {
+  //   id: 'black-forest-labs/FLUX.1-dev',
+  //   provider: 'silicon',
+  //   name: 'FLUX.1 Dev',
+  //   group: 'FLUX'
+  // },
+  // {
+  //   id: 'black-forest-labs/FLUX.1-pro',
+  //   provider: 'silicon',
+  //   name: 'FLUX.1 Pro',
+  //   group: 'FLUX'
+  // },
+  // {
+  //   id: 'Pro/black-forest-labs/FLUX.1-schnell',
+  //   provider: 'silicon',
+  //   name: 'FLUX.1 Schnell Pro',
+  //   group: 'FLUX'
+  // },
+  // {
+  //   id: 'LoRA/black-forest-labs/FLUX.1-dev',
+  //   provider: 'silicon',
+  //   name: 'FLUX.1 Dev LoRA',
+  //   group: 'FLUX'
+  // },
+  // {
+  //   id: 'deepseek-ai/Janus-Pro-7B',
+  //   provider: 'silicon',
+  //   name: 'Janus-Pro-7B',
+  //   group: 'deepseek-ai'
+  // },
+  // {
+  //   id: 'stabilityai/stable-diffusion-3-5-large',
+  //   provider: 'silicon',
+  //   name: 'Stable Diffusion 3.5 Large',
+  //   group: 'Stable Diffusion'
+  // },
+  // {
+  //   id: 'stabilityai/stable-diffusion-3-5-large-turbo',
+  //   provider: 'silicon',
+  //   name: 'Stable Diffusion 3.5 Large Turbo',
+  //   group: 'Stable Diffusion'
+  // },
+  // {
+  //   id: 'stabilityai/stable-diffusion-3-medium',
+  //   provider: 'silicon',
+  //   name: 'Stable Diffusion 3 Medium',
+  //   group: 'Stable Diffusion'
+  // },
+  // {
+  //   id: 'stabilityai/stable-diffusion-2-1',
+  //   provider: 'silicon',
+  //   name: 'Stable Diffusion 2.1',
+  //   group: 'Stable Diffusion'
+  // },
+  // {
+  //   id: 'stabilityai/stable-diffusion-xl-base-1.0',
+  //   provider: 'silicon',
+  //   name: 'Stable Diffusion XL Base 1.0',
+  //   group: 'Stable Diffusion'
+  // }
 ]
 
 export const TEXT_TO_IMAGES_MODELS_SUPPORT_IMAGE_ENHANCEMENT = [
