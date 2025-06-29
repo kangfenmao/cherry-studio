218dcc222926ec2907b4d6e65d238ab4c5850142
Yiyang Suen <suenyiyang+github@gmail.com>
2025-06-30 00:01:28 +0800

    fix: textarea not resizing back after clearing long input (#7609) (#7632)
    * fix: textarea not resizing back after clearing long input (#7609)
    
    * fix: text area auto size only when not dragged

--- src/renderer/src/pages/home/Inputbar/Inputbar.tsx
+++ src/renderer/src/pages/home/Inputbar/Inputbar.tsx
@@ -788,6 +788,7 @@ const Inputbar: FC<Props> = ({ assistant: _assistant, setActiveTopic, topic }) =
             variant="borderless"
             spellCheck={enableSpellCheck}
             rows={2}
+            autoSize={textareaHeight ? false : { minRows: 2, maxRows: 20 }}
             ref={textareaRef}
             style={{
               fontSize,
