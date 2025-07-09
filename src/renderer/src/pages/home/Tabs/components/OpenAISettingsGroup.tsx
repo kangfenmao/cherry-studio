c7ab71f01f789c134398566e8f9c0ac495e887f9
kangfenmao <kangfenmao@qq.com>
2025-07-09 20:38:18 +0800

    refactor(OpenAISettingsGroup): simplify component structure and remove styled components
    - Removed unused imports and the StyledSelect component, replacing it with a standard Selector for improved clarity.
    - Streamlined the layout by eliminating unnecessary styles, enhancing maintainability and readability of the code.

--- src/renderer/src/pages/home/Tabs/components/OpenAISettingsGroup.tsx
+++ src/renderer/src/pages/home/Tabs/components/OpenAISettingsGroup.tsx
@@ -4,12 +4,11 @@ import { CollapsibleSettingGroup } from '@renderer/pages/settings/SettingGroup'
 import { RootState, useAppDispatch } from '@renderer/store'
 import { setOpenAIServiceTier, setOpenAISummaryText } from '@renderer/store/settings'
 import { OpenAIServiceTier, OpenAISummaryText } from '@renderer/types'
-import { Select, Tooltip } from 'antd'
+import { Tooltip } from 'antd'
 import { CircleHelp } from 'lucide-react'
 import { FC, useCallback, useEffect, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
-import styled from 'styled-components'
 
 interface Props {
   isOpenAIReasoning: boolean
@@ -121,13 +120,11 @@ const OpenAISettingsGroup: FC<Props> = ({
                   <CircleHelp size={14} style={{ marginLeft: 4 }} color="var(--color-text-2)" />
                 </Tooltip>
               </SettingRowTitleSmall>
-              <StyledSelect
+              <Selector
                 value={summaryText}
-                style={{ width: 135 }}
                 onChange={(value) => {
                   setSummaryText(value as OpenAISummaryText)
                 }}
-                size="small"
                 options={summaryTextOptions}
               />
             </SettingRow>
@@ -138,13 +135,5 @@ const OpenAISettingsGroup: FC<Props> = ({
     </CollapsibleSettingGroup>
   )
 }
-
-const StyledSelect = styled(Select)`
-  .ant-select-selector {
-    border-radius: 15px !important;
-    padding: 4px 10px !important;
-    height: 26px !important;
-  }
-`
 
 export default OpenAISettingsGroup
