f8c6b5c05fe4a2ca91dbe6ebf51d745985236160
自由的世界人 <3196812536@qq.com>
2025-07-10 15:09:59 +0800

    Fix translation key for unlimited backups label (#7987)
    Updated the translation key for the 'unlimited' backups option in WebDavSettings to use the correct namespace.

--- src/renderer/src/pages/settings/DataSettings/WebDavSettings.tsx
+++ src/renderer/src/pages/settings/DataSettings/WebDavSettings.tsx
@@ -202,7 +202,7 @@ const WebDavSettings: FC = () => {
           onChange={onMaxBackupsChange}
           disabled={!webdavHost}
           options={[
-            { label: t('settings.data.webdav.maxBackups.unlimited'), value: 0 },
+            { label: t('settings.data.local.maxBackups.unlimited'), value: 0 },
             { label: '1', value: 1 },
             { label: '3', value: 3 },
             { label: '5', value: 5 },
