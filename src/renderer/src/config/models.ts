ff58efcbf3420e34e6e49a203604eaa62bff1dd6
Peter Wang <59994316+PeterWang-dev@users.noreply.github.com>
2025-08-08 00:38:24 +0800

    fix: make regex of gemma3 can match ollama's format (#3847 #6626) (#8941)

--- src/renderer/src/config/models.ts
+++ src/renderer/src/config/models.ts
@@ -194,7 +194,7 @@ const visionAllowedModels = [
   'gemma-3(?:-[\\w-]+)',
   'doubao-seed-1[.-]6(?:-[\\w-]+)?',
   'kimi-thinking-preview',
-  `gemma3(?:-[\\w-]+)`,
+  `gemma3(?:[-:\\w]+)?`,
   'kimi-vl-a3b-thinking(?:-[\\w-]+)?',
   'llama-guard-4(?:-[\\w-]+)?',
   'llama-4(?:-[\\w-]+)?',
