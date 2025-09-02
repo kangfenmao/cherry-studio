2c0391da81faf45bbe910c8ef164cdd9418cfd28
beyondkmp <beyondkmp@gmail.com>
2025-09-02 15:51:23 +0800

    chore: update vite down to 7.1.5 (#9794)
    chore: update electron.vite.config.ts and yarn.lock for dependency management
    
    - Added additional external dependencies in electron.vite.config.ts to improve build configuration.
    - Updated multiple package versions in yarn.lock, including @napi-rs/wasm-runtime, @oxc-project/runtime, and @rolldown packages to their latest beta versions for better compatibility and performance.
    - Adjusted fdir and picomatch versions to ensure alignment with the latest features and fixes.

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -26,7 +26,20 @@ export default defineConfig({
     },
     build: {
       rollupOptions: {
-        external: ['@libsql/client', 'bufferutil', 'utf-8-validate'],
+        external: [
+          '@libsql/client',
+          'bufferutil',
+          'utf-8-validate',
+          'jsdom',
+          'electron',
+          'graceful-fs',
+          'selection-hook',
+          '@napi-rs/system-ocr',
+          '@strongtz/win32-arm64-msvc',
+          'os-proxy-config',
+          'sharp',
+          'turndown'
+        ],
         output: {
           manualChunks: undefined, // 彻底禁用代码分割 - 返回 null 强制单文件打包
           inlineDynamicImports: true // 内联所有动态导入，这是关键配置
