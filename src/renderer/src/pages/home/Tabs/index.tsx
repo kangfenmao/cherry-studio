94374e7de236657e2941b7a4aa31c89e17d1ecab
Konv Suu <2583695112@qq.com>
2025-09-02 19:09:11 +0800

    fix: tabs 高度不足导致 border 样式不能占满父元素 (#9780)
    * fix: tabs 高度不足导致 border 样式不能占满父元素
    
    * Revert

--- src/renderer/src/pages/home/Tabs/index.tsx
+++ src/renderer/src/pages/home/Tabs/index.tsx
@@ -155,9 +155,12 @@ const Container = styled.div`
   flex-direction: column;
   max-width: var(--assistants-width);
   min-width: var(--assistants-width);
+  height: calc(100vh - var(--navbar-height));
+
   &.right {
     height: calc(100vh - var(--navbar-height));
   }
+
   [navbar-position='left'] & {
     background-color: var(--color-background);
   }
