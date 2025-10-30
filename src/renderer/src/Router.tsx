dd8690b59281817071db9aab93350e9efb29528f
SuYao <sy20010504@gmail.com>
2025-10-30 16:41:07 +0800

    feat: 添加路由懒加载组件以优化页面加载性能 (#11042)

--- src/renderer/src/Router.tsx
+++ src/renderer/src/Router.tsx
@@ -1,6 +1,7 @@
 import '@renderer/databases'
 
-import { FC, useMemo } from 'react'
+import { Spinner } from '@heroui/react'
+import { FC, lazy, Suspense, useMemo } from 'react'
 import { HashRouter, Route, Routes } from 'react-router-dom'
 
 import Sidebar from './components/app/Sidebar'
@@ -8,18 +9,25 @@ import { ErrorBoundary } from './components/ErrorBoundary'
 import TabsContainer from './components/Tab/TabContainer'
 import NavigationHandler from './handler/NavigationHandler'
 import { useNavbarPosition } from './hooks/useSettings'
-import CodeToolsPage from './pages/code/CodeToolsPage'
-import FilesPage from './pages/files/FilesPage'
-import HomePage from './pages/home/HomePage'
-import KnowledgePage from './pages/knowledge/KnowledgePage'
-import LaunchpadPage from './pages/launchpad/LaunchpadPage'
-import MinAppPage from './pages/minapps/MinAppPage'
-import MinAppsPage from './pages/minapps/MinAppsPage'
-import NotesPage from './pages/notes/NotesPage'
-import PaintingsRoutePage from './pages/paintings/PaintingsRoutePage'
-import SettingsPage from './pages/settings/SettingsPage'
-import AssistantPresetsPage from './pages/store/assistants/presets/AssistantPresetsPage'
-import TranslatePage from './pages/translate/TranslatePage'
+
+const HomePage = lazy(() => import('./pages/home/HomePage'))
+const AssistantPresetsPage = lazy(() => import('./pages/store/assistants/presets/AssistantPresetsPage'))
+const PaintingsRoutePage = lazy(() => import('./pages/paintings/PaintingsRoutePage'))
+const TranslatePage = lazy(() => import('./pages/translate/TranslatePage'))
+const FilesPage = lazy(() => import('./pages/files/FilesPage'))
+const NotesPage = lazy(() => import('./pages/notes/NotesPage'))
+const KnowledgePage = lazy(() => import('./pages/knowledge/KnowledgePage'))
+const MinAppPage = lazy(() => import('./pages/minapps/MinAppPage'))
+const MinAppsPage = lazy(() => import('./pages/minapps/MinAppsPage'))
+const CodeToolsPage = lazy(() => import('./pages/code/CodeToolsPage'))
+const SettingsPage = lazy(() => import('./pages/settings/SettingsPage'))
+const LaunchpadPage = lazy(() => import('./pages/launchpad/LaunchpadPage'))
+
+const RouterFallback: FC = () => (
+  <div className="flex h-full w-full items-center justify-center">
+    <Spinner color="primary" size="lg" label="Loading" />
+  </div>
+)
 
 const Router: FC = () => {
   const { navbarPosition } = useNavbarPosition()
@@ -27,20 +35,22 @@ const Router: FC = () => {
   const routes = useMemo(() => {
     return (
       <ErrorBoundary>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/store" element={<AssistantPresetsPage />} />
-          <Route path="/paintings/*" element={<PaintingsRoutePage />} />
-          <Route path="/translate" element={<TranslatePage />} />
-          <Route path="/files" element={<FilesPage />} />
-          <Route path="/notes" element={<NotesPage />} />
-          <Route path="/knowledge" element={<KnowledgePage />} />
-          <Route path="/apps/:appId" element={<MinAppPage />} />
-          <Route path="/apps" element={<MinAppsPage />} />
-          <Route path="/code" element={<CodeToolsPage />} />
-          <Route path="/settings/*" element={<SettingsPage />} />
-          <Route path="/launchpad" element={<LaunchpadPage />} />
-        </Routes>
+        <Suspense fallback={<RouterFallback />}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/store" element={<AssistantPresetsPage />} />
+            <Route path="/paintings/*" element={<PaintingsRoutePage />} />
+            <Route path="/translate" element={<TranslatePage />} />
+            <Route path="/files" element={<FilesPage />} />
+            <Route path="/notes" element={<NotesPage />} />
+            <Route path="/knowledge" element={<KnowledgePage />} />
+            <Route path="/apps/:appId" element={<MinAppPage />} />
+            <Route path="/apps" element={<MinAppsPage />} />
+            <Route path="/code" element={<CodeToolsPage />} />
+            <Route path="/settings/*" element={<SettingsPage />} />
+            <Route path="/launchpad" element={<LaunchpadPage />} />
+          </Routes>
+        </Suspense>
       </ErrorBoundary>
     )
   }, [])
